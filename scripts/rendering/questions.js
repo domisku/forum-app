9ae3810811f1f16383cfb7550a0c35f202c4632a
Dominykas <84922120+domisku@users.noreply.github.com>
2022-02-12 14:05:36 +0200

    fixed a bug where going to the next results page, old results would stay

--- scripts/rendering/questions.js
+++ scripts/rendering/questions.js
@@ -6,9 +6,16 @@ export default async function renderQuestions(questions) {
 
   const users = await fetchFromDB("users");
 
+  clearMainContent();
+
   for (let entry of questions) {
     const author = users.find((user) => user.userId === entry.userId);
 
     renderQuestion(entry, author);
   }
+}
+
+function clearMainContent() {
+  const mainContent = document.querySelector(".main-content");
+  if (mainContent.childElementCount) mainContent.innerHTML = "";
 }
