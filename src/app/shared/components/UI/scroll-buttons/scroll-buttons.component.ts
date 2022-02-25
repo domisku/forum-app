ebf69903ce6fa70f06b8fd5e24b058c7fe2de191
Dominykas <84922120+domisku@users.noreply.github.com>
2022-02-25 14:01:16 +0200

    reworked scroll event listener in scroll buttons component to use HostListener instead

--- src/app/shared/components/UI/scroll-buttons/scroll-buttons.component.ts
+++ src/app/shared/components/UI/scroll-buttons/scroll-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { ScrollService } from 'src/app/core/recources/services/scroll.service';
 
@@ -7,33 +7,32 @@ import { ScrollService } from 'src/app/core/recources/services/scroll.service';
   templateUrl: './scroll-buttons.component.html',
   styleUrls: ['./scroll-buttons.component.scss'],
 })
-export class ScrollButtonsComponent implements OnInit {
+export class ScrollButtonsComponent {
   showScrollButtons = false;
   scrollThreshold = 200;
   faAngleDown = faAngleDown;
   faAngleUp = faAngleUp;
 
-  constructor(private scrollService: ScrollService) {}
-
-  ngOnInit(): void {
+  @HostListener('window:scroll') onScrollEvent() {
     let scrolling = false;
 
-    document.addEventListener('scroll', () => {
-      if (!scrolling) {
-        setTimeout(() => {
-          scrolling = false;
-          let scrollPosition = window.scrollY;
-          if (scrollPosition > this.scrollThreshold) {
-            this.showScrollButtons = true;
-          } else if (scrollPosition < this.scrollThreshold) {
-            this.showScrollButtons = false;
-          }
-        }, 300);
+    //scroll throttling
+    if (!scrolling) {
+      setTimeout(() => {
+        scrolling = false;
+        let scrollPosition = window.scrollY;
+        if (scrollPosition > this.scrollThreshold) {
+          this.showScrollButtons = true;
+        } else if (scrollPosition < this.scrollThreshold) {
+          this.showScrollButtons = false;
+        }
+      }, 300);
 
-        scrolling = true;
-      }
-    });
+      scrolling = true;
+    }
   }
+
+  constructor(private scrollService: ScrollService) {}
 
   scrollToPageTop() {
     this.scrollService.scrollToPageTop();
